b485e827ea1f4efca6207b3cc1abef8ee9472f70
Andres Alvarez <juan.alvarez1989@gmail.com>
2024-09-20 19:19:07 -0300

    funciones: first pls length

--- funciones_basicas_II.py
+++ funciones_basicas_II.py
@@ -1,4 +1,4 @@
-# . Cuenta regresiva. crea una función que acepte un número como entrada. Devuelve una nueva lista que cuenta hacia atrás en uno, desde el número (como el elemento 0) hasta 0 (como el último elemento).
+# 1. Cuenta regresiva. crea una función que acepte un número como entrada. Devuelve una nueva lista que cuenta hacia atrás en uno, desde el número (como el elemento 0) hasta 0 (como el último elemento).
 
 def cuenta_regresiva (numero):
     cuenta =[]
@@ -15,4 +15,11 @@ def imprimir_y_devolver (lista):
     return lista[1]
 
 imprimir_y_devolver([1,2])
+
+# 3. First Plus Length : crea una función que acepta una lista y devuelve la suma del primer valor de la lista más la longitud de la lista. Ejemplo: first_plus_length ([1,2,3,4,5]) debería devolver 6 (primer valor: 1 + longitud: 5)
+
+def first_plus_length(lista):
+    return lista[0]+len(lista)
+
+print(first_plus_length([1,2,3,4,5]))
 
