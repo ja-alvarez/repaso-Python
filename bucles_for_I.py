f06161a1eebeec1649f30c9cd9cd1eb2697e8700
Andres Alvarez <juan.alvarez1989@gmail.com>
2024-09-19 21:28:12 -0300

    Multiplos de 5

--- bucles_for_I.py
+++ bucles_for_I.py
@@ -1,3 +1,9 @@
 # 1. imprime todos los enteros del 0 al 150.
 for numero in range(0, 151):
     print (numero)
+
+
+#2. imprime todos los múltiplos de 5 de 5 a 1,000
+for multiplo_cinco in range(5, 1001, 5):
+    print(multiplo_cinco)
+
