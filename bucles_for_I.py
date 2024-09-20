4fc4d01516617322719cef4831583a357e010260
Andres Alvarez <juan.alvarez1989@gmail.com>
2024-09-19 21:28:55 -0300

    enteros 1 - 100

--- bucles_for_I.py
+++ bucles_for_I.py
@@ -7,3 +7,11 @@ for numero in range(0, 151):
 for multiplo_cinco in range(5, 1001, 5):
     print(multiplo_cinco)
 
+# 3. imprime enteros del 1 al 100. Si es divisible por 5, imprima "Coding" en su lugar. Si es divisible por 10, imprima "Coding Dojo".
+for entero in range(1, 101):
+    if entero % 10 == 0:
+        print('Coding Dojo')
+    elif entero % 5 == 0:
+        print('Coding')
+    else:
+        print(entero)
