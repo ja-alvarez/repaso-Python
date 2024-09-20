c41019390b304816eb29693ee9fdb572b6d36a91
Andres Alvarez <juan.alvarez1989@gmail.com>
2024-09-20 19:38:03 -0300

    funciones: imprimir length y devolver valores mayores al segundo

--- funciones_basicas_II.py
+++ funciones_basicas_II.py
@@ -23,3 +23,19 @@ def first_plus_length(lista):
 
 print(first_plus_length([1,2,3,4,5]))
 
+# 4. Valores mayores que el segundo : escribe una función que acepte una lista y crea una nueva lista que contenga solo los valores de la lista original que sean mayores que su segundo valor. Imprima cuántos valores son y luego devuelva la nueva lista. Si la lista tiene menos de 2 elementos, haga que la función devuelva False
+# Ejemplo: values_greater_than_second ([5,2,3,2,1,4]) debería imprimir 3 y devolver [5,3,4]
+# Ejemplo: values_greater_than_second ([3]) debería devolver False
+
+def values_greater_than_second(lista):
+    nueva_lista = []
+    if len(lista)<2:
+        return False
+    else:
+        for i in range(0, len(lista)):
+            if lista[i]>lista[1]:
+                nueva_lista.append(lista[i])
+        print(len(nueva_lista)) # imprime length de la nueva lista
+        return nueva_lista
+print(values_greater_than_second([5,2,3,2,1,4]))
+
