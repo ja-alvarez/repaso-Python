8e2253e9d1cb096a7cb26a8150cea8742f12938a
Andres Alvarez <juan.alvarez1989@gmail.com>
2024-09-20 21:47:51 -0300

    Funcion intermedia

---
+++ funciones_intermedias_I.py
@@ -0,0 +1,14 @@
+# escribir una sola función, randInt() que tome hasta 2 argumentos.
+# Si no se proporcionan argumentos, la función debería devolver un entero aleatorio entre 0 y 100.
+# Si solo se proporciona un número máximo, la función debe devolver un número entero aleatorio entre 0 y el número máximo.
+# Si solo se proporciona un número mínimo, la función debe devolver un número entero aleatorio entre el número mínimo y 100
+# Si se proporcionan un número mínimo y máximo, la función debe devolver un número entero aleatorio entre esos 2 valores.
+
+import random
+
+def randInt(min = 0, max = 100):
+    num = round(random.random()*(max-min)+min)
+    return num
+
+print(randInt(max=5))
+print(randInt(min=50, max=500))
